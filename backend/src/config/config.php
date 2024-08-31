146672590570b19cab9dd3237a20e29fdd0e7960
elantonio12 <luisasv1106@gmail.com>
2024-08-30 19:14:46 -0600

    Update config.php

--- backend/src/config/config.php
+++ backend/src/config/config.php
@@ -1,17 +1,23 @@
 <?php
-define('SERVIDOR',localhost);
-define('USUARIO','root');
-define('PASSWORD','');
-define('BD',"sistemagestionescolar13");
-define('APP_NAME','Sistema de Gestion Escolar');
-define('APP_URL','http://localhost/config.php')
-$servidor="mysql:dbname=".BD.";host=".SERVIDOR;
-try{
-$conexion=new PDO($servidor,USUARIO,PASSWORD);
+define('SERVIDOR', 'localhost'); 
+define('USUARIO', 'root');
+define('PASSWORD', '');
+define('BD', 'sistemagestionescolar13');
+define('APP_NAME', 'Sistema de Gestion Escolar');
+define('APP_URL', 'http://localhost/config.php'); 
 
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
 
-}catch(PDOException $error){
-    echo "conexion erronea".$error;
+try {
+   
+    $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    
+    // Pasamos las opciones en el constructor de PDO
+    $conexion = new PDO($servidor, USUARIO, PASSWORD, $opciones);
+    echo "Conexión exitosa a la base de datos";
+    
+} catch (PDOException $error) {
+    echo "Conexión errónea: " . $error->getMessage();
 }
 
 ?>

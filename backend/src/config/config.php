7375fce9271baf1dd60652cd901f844649cdd330
elantonio12 <luisasv1106@gmail.com>
2024-08-30 19:07:04 -0600

    Update config.php

--- backend/src/config/config.php
+++ backend/src/config/config.php
@@ -5,7 +5,7 @@ define('PASSWORD','');
 define('BD',"sistemagestionescolar13");
 define('APP_NAME','Sistema de Gestion Escolar');
 define('APP_URL','http://localhost/config.php')
-$servidor="mysql:dbname="BD.";host=".SERVIDOR;
+$servidor="mysql:dbname=".BD.";host=".SERVIDOR;
 try{
 $conexion=new PDO($servidor,USUARIO,PASSWORD);
 

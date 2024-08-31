52e6ab13893724a43dedd0beb2ae0474aae09701
elantonio12 <luisasv1106@gmail.com>
2024-08-31 02:46:43 -0600

    Update validate_login.php

--- backend/src/controllers/validate_login.php
+++ backend/src/controllers/validate_login.php
@@ -1,19 +1,25 @@
 <?php
-include('../config/config.php'); 
+include('../config/config.php'); //  conexión a la base de datos
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Obtener datos del formulario
+    // Obtener datos del formulario y sanitizarlos
     $user_type = htmlspecialchars($_POST['user_type']);
     $user = htmlspecialchars($_POST['username']);
-    $pass = $_POST['password'];
+    $pass = $_POST['password']; // La contraseña se debe verificar más tarde
 
     try {
-        // Consulta SQL para verificar usuario
+        // Consulta SQL para verificar el usuario
         $sql = "SELECT * FROM usuarios WHERE cargo = :user_type AND nombres = :username";
         $stmt = $conexion->prepare($sql);
         $stmt->bindParam(':user_type', $user_type);
         $stmt->bindParam(':username', $user);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Mostrar datos recuperados para depuración
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
 
         if ($result) {
             // Verificar la contraseña

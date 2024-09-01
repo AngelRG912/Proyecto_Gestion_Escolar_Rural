0af06cb8639aa81006c279d94c338d5748dcaf29
Angel Ruiz <anonimus09122004@gmail.com>
2024-08-31 18:44:17 -0600

    uptade necesario

--- frontend/src/pages/login.php
+++ frontend/src/pages/login.php
@@ -1,38 +1,26 @@
-<?php
-  $userType = $_POST['user_type'];
-  $claseFondo = "../assets/img/Imagen.jpeg";
-  if ($userType == 'alumno') {
-    header("Location: http://localhost:3000/frontend/src/pages/login.php");
-    $claseFondo = "fondo-alumno";
-  } elseif ($userType == 'profesor') {
-    header("Location: http://localhost:3000/frontend/src/pages/login.php");
-    $claseFondo = "fondo-alumno";
-  } else {
-    header("Location: http://localhost:3000/frontend/src/pages/login.php");
-    $claseFondo = "fondo-alumno";
-  }
-?>
-
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>Login</title>
-    <!--Conectar el archivo CSS-->
-    <link rel="stylesheet" href="../styles/login.css" />
-  </head>
-  <body>
-    <h2>Login de </h2>
-    <form action="../../../backend/src/controllers/validate_login.php" method="post">
-      <input type="hidden" name="user_type" value=""/>
-      <label for="username">Usuario:</label>
-      <input type="text" id="username" name="username" required />
-      <br />
-      <label for="password">Contraseña:</label>
-      <input type="password" id="password" name="password" required />
-      <br />
-      <button type="submit">Ingresar</button>
-    </form>
-    <p class="register-link">¿No tienes una cuenta?<a href="../../../backend/src/controllers/register_user.php">Regístrate aquí</a></p>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Login</title>
+  <!--Conectar el archivo CSS-->
+  <link rel="stylesheet" href="../styles/login.css" />
+</head>
+
+<body class="<?php echo htmlspecialchars($_POST['user_type']); ?>">
+  <h2>Login de <?php echo htmlspecialchars($_POST['user_type']); ?></h2>
+  <form action="../../../backend/src/controllers/validate_login.php" method="post">
+    <input type="hidden" name="user_type" value="<?php echo htmlspecialchars($_POST['user_type']); ?>" />
+    <label for="username">Usuario:</label>
+    <input type="text" id="username" name="username" required />
+    <br />
+    <label for="password">Contraseña:</label>
+    <input type="password" id="password" name="password" required />
+    <br />
+    <button type="submit">Ingresar</button>
+  </form>
+  <p class="register-link">¿No tienes una cuenta?<a href="../../../backend/src/controllers/register_user.php">Regístrate aquí</a></p>
+</body>
+
 </html>
